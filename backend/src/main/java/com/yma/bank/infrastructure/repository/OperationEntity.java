6967af7ab3159e8f71672c0d4eb8e9ef068cae74
youmassa <y.massaoudi@yahoo.fr>
2025-03-15 15:02:12 +0100

    BankAccount : Upgrade spring boot from 2.6.2 to 3.2.2 and java from 11 to 17

--- backend/src/main/java/com/yma/bank/infrastructure/repository/OperationEntity.java
+++ backend/src/main/java/com/yma/bank/infrastructure/repository/OperationEntity.java
@@ -4,10 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
--- backend/src/main/java/com/yma/bank/infrastructure/repository/OperationRepositoryExtendedImpl.java
+++ backend/src/main/java/com/yma/bank/infrastructure/repository/OperationRepositoryExtendedImpl.java
@@ -6,7 +6,7 @@ import com.yma.bank.domain.services.OperationRepositoryExtended;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import java.time.LocalDateTime;
 import java.util.List;
 
